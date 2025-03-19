64529007bf8c6a24de7c53655e19130a8572a8fe
김창민 <changmin@gimchangmin-ui-MacBookPro.local>
2025-03-19 15:01:39 +0900

    fix: kotlin 버전 및 JavaLanguageVersion 세팅 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.8.0"
-    id("org.springframework.boot") version "3.2.0"
-    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("jvm") version "1.9.22" // Kotlin JVM 버전을 Java 21에 맞춤
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 allprojects {
@@ -14,6 +14,12 @@ subprojects {
     apply(plugin = "java")
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(21)) // Java 21 설정
+        }
+    }
 
     dependencies {
         implementation("org.springframework.boot:spring-boot-starter-web")
